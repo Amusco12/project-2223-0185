08474db4195416de2fb2edd62ce95e9227dfddb9
Amusco12 <152966847+Amusco12@users.noreply.github.com>
2023-12-12 22:29:48 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Amubieya ifeoluwa james!</Text>
+      <Text>EES/22/23/0185!</Text>
+      <Text>This is my first project!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
